355e370019c51ec71f7b5c0d2f7664243bd2e11c
claudiamihaela <tclaudiamihaela@gmail.com>
2023-02-03 17:53:57 +0200

    changed colors, photos and navbar

--- team/team.js
+++ team/team.js
@@ -1,11 +1,11 @@
 
 let textElement = document.querySelector('p')
 textElement.addEventListener('mouseover', () =>{
-    document.getElementById('text').style.fontSize= '40px';
-    document.getElementById('text').style.color= 'rgb(236, 18, 236)';
+    document.getElementById('text').style.fontSize= '35px';
+    document.getElementById('text').style.color= '#000';
 });
 
 textElement.addEventListener('mouseout', () =>{
     document.getElementById('text').style.fontSize= '30px';
-    document.getElementById('text').style.color= 'rgb(14, 1, 26)';
+    document.getElementById('text').style.color= '#fff';
 });
